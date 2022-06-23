811cbf3bc52f051e26f2be46669cf73a713f1c24
XHLin-gamer <73104227+XHLin-gamer@users.noreply.github.com>
2022-06-23 13:50:30 +0800

    2022年6月23日13点50分

---
+++ ray_module.py
@@ -0,0 +1,170 @@
+from random import random
+import taichi as ti
+
+
+use_f64 = False
+
+
+float_type = ti.f32
+if use_f64:
+    float_type = ti.f64
+vec3 = ti.types.vector(3, float_type)
+
+# @ti.struct_class
+@ti.data_oriented
+class ray:
+    origin: vec3
+    direction: vec3
+    # @ti.func
+    def __init__(self, origin:float_type, direction:float_type):
+        self.origin = origin
+        self.direction = direction.normalized()
+
+    @ti.func
+    def at(self, t: float_type) -> ti.Vector:
+        return self.origin + t * self.direction
+
+
+@ti.struct_class
+class sphere:
+    center: vec3
+    radius: float_type
+    material: int
+    # lambertian 0
+    # metal 1
+    # dielectric 2
+    color: vec3
+    fuzz: float_type
+    etai_over_etat: float_type
+
+    @ti.func
+    def hit(self, ray, t_min, t_max):
+        oc = ray.origin - self.center
+        a = ray.direction.dot(ray.direction)
+        half_b = oc.dot(ray.direction)
+        c = oc.dot(oc) - self.radius**2
+
+        discriminant = half_b**2 - a*c
+        is_hit = False
+        root: float_type = 0.0
+        if discriminant > 0:
+            is_hit = True
+            sqrtd = ti.sqrt(discriminant)
+
+            root = (-half_b - sqrtd) / a
+            if root < t_min or t_max < root:
+                root = (-half_b + sqrtd) / a
+                if root < t_min or t_max < root:
+                    is_hit = False
+        position = ray.at(root)
+        normal = (position - self.center)/self.radius
+
+        return is_hit, root, position, normal, self.color, self.material, self.fuzz, self.etai_over_etat
+
+
+@ti.func
+def random_init_sphere():
+    a = ti.random() * 3.1415926
+    b = ti.random() * 3.1415926
+    return ti.Vector([ti.cos(a)*ti.cos(b), ti.sin(a)*ti.cos(b), ti.sin(b)])
+
+
+@ti.data_oriented
+class hittable_list:
+    def __init__(self, max_sphere_nums):
+        self.sphere_field = sphere.field(shape=(max_sphere_nums,))
+        self.sphere_loaded = 0
+
+    def add_sphere(self, center, radius, material, color=ti.Vector([.2, .3, .4]), fuzz=0, etai_over_etat=0):
+        self.sphere_field[self.sphere_loaded].center = center
+        self.sphere_field[self.sphere_loaded].radius = radius
+        self.sphere_field[self.sphere_loaded].material = material
+        self.sphere_field[self.sphere_loaded].color = color
+        self.sphere_field[self.sphere_loaded].fuzz = fuzz
+        self.sphere_field[self.sphere_loaded].etai_over_etat = etai_over_etat
+        self.sphere_loaded += 1
+
+    @ti.func
+    def hit(self, ray, t_min, t_max):
+        root: float_type = 2.0**20
+        position = ti.Vector([0.0, 0.0, 0.0], dt=float_type)
+        normal = ti.Vector([0.0, 0.0, 0.0], dt=float_type)
+        color = ti.Vector([0.0, 0.0, 0.0], dt=float_type)
+        material = -1
+        is_hit = False
+        front_face = True
+        fuzz: float_type = 0.0
+        etai_over_etat:float_type = 0.0
+        for i in range(self.sphere_loaded):
+            _is_hit, _root, _position, _normal, _color, _material, _fuzz, _etai_over_etat = self.sphere_field[i].hit(
+                ray, t_min, t_max)
+            if _is_hit and root > _root:
+                is_hit = _is_hit
+                root = _root
+                position = _position
+                normal = _normal
+                color = _color
+                material = _material
+                fuzz = _fuzz
+                etai_over_etat = _etai_over_etat
+                if ray.direction.dot(normal) > 0.0:
+                    front_face = False
+                    normal = -normal
+                else:
+                    front_face = True
+
+        return is_hit, root, position, normal, front_face, color, material, fuzz, etai_over_etat
+
+
+@ti.func
+def reflect(v, n):
+    return v - 2*v.dot(n)*n
+
+
+@ti.func
+def refract(uv, n, etai_over_etat):
+    cos_theta = ti.min(-n.dot(uv), 1.0)
+    r_out_perp = etai_over_etat * (uv + cos_theta*n)
+    r_out_parallel = -ti.sqrt(ti.abs(1.0 - r_out_perp.dot(r_out_perp))) * n
+    return r_out_perp + r_out_parallel
+
+
+@ti.func
+def reflectance(cosine, ref_idx):
+    r0 = (1-ref_idx) / (1+ref_idx)
+    r0 = r0**2
+    return r0 + (1 - r0)*((1-cosine)**5)
+
+
+@ti.func
+def random_in_unit_disk():
+    ttt = ti.random() * 3.1415926 * 2
+    return ti.Vector([ti.cos(ttt), ti.sin(ttt), 0.0])
+
+
+@ti.data_oriented
+class camera:
+    def __init__(self, look_from, look_at, vup, vfov, aspect_ratio, aperture=1.0, focus_dist=1.0):
+        theta = vfov/180.0*3.14159265
+        h = ti.tan(theta/2)
+        viewport_height = 2.0*h
+        viewport_width = aspect_ratio * viewport_height
+
+        self.w = (look_from - look_at).normalized()
+        self.u = vup.cross(self.w).normalized()
+        self.v = self.w.cross(self.u)
+
+        self.origin = look_from
+        self.horizental = viewport_width * self.u * focus_dist
+        self.vertical = viewport_height * self.v * focus_dist
+        self.lower_left_corner = self.origin - \
+            self.horizental/2 - self.vertical/2 - self.w*focus_dist
+        self.lens_radius = aperture / 2.0
+
+    @ti.func
+    def get_ray(self, s, t):
+        rd = self.lens_radius * random_in_unit_disk()
+        offset = self.u*rd[0] + self.v*rd[1]
+
+        return ray(origin=self.origin + offset,
+                   direction=self.lower_left_corner + s*self.horizental + t*self.vertical - self.origin - offset)
---
+++ weekend.py
@@ -0,0 +1,169 @@
+from random import random
+from ray_module import *
+import taichi as ti
+import time
+
+ti.init(arch=ti.gpu)
+res = 720, 500
+
+# rays pools
+# BVH
+
+output_image = False
+output_image_target_iteration = 1000
+
+color_buffer = ti.Vector.field(3, dtype=float_type, shape=res)
+frame_buffer = ti.Vector.field(3, dtype=float_type, shape=res)
+iteration = ti.field(dtype=int, shape=())
+iteration[None] = 0
+viewport_height = 2.0
+aspect_ratio = res[0] / res[1]
+viewport_width = aspect_ratio * viewport_height
+sampels_per_pixel = ti.field(dtype=ti.int32, shape=())
+sampels_per_pixel[None] = 4
+depth = 10
+# camera
+# focus_lenth = 1.0
+# origin = ti.Vector([0.0, 0.0, 0.0])
+# horizontal = ti.Vector([viewport_width, 0, 0])
+# vertical = ti.Vector([0, viewport_height, 0])
+# lower_left_corner = origin - 0.5 * horizontal - \
+#     0.5*vertical - ti.Vector([0, 0, focus_lenth])
+
+# test_sphere = sphere(center=ti.Vector([0, 0, -1]), radius=0.5)
+# another_sphere = sphere(center=ti.Vector([0, -100.5, -1]), radius=100.0)
+
+scene = hittable_list(max_sphere_nums=2**9)
+
+
+def init_scene():
+    # ground
+    scene.add_sphere(center=ti.Vector(
+        [0, -1000.0, -1]), radius=1000.0, material=0, color=ti.Vector([0.5, 0.5, 0.5]))
+    for _a in range(22):
+        for _b in range(22):
+            rrr = random()
+            center = ti.Vector(
+                [_a - 11.0 + 0.9*random(), 0.2, _b - 11.0 + 0.9*random()])
+            if (center - ti.Vector([4.0, 0.2, 0.0])).dot(center - ti.Vector([4.0, 0.2, 0.0])) > 0.8:
+                if rrr < 0.8:
+                    color = ti.Vector([random(), random(), random()])
+                    scene.add_sphere(center=center, radius=0.2,
+                                     material=0, color=color)
+                elif rrr < 0.95:
+                    color = ti.Vector(
+                        [random(), random(), random()])*0.5 + ti.Vector([0.5, 0.5, 0.5])
+                    fuzz = random()*0.5
+                    scene.add_sphere(center=center, radius=.2,
+                                     material=1, fuzz=fuzz)
+                else:
+                    scene.add_sphere(center=center, radius=0.2,
+                                     material=2, etai_over_etat=1.5)
+
+    scene.add_sphere(ti.Vector([0.0, 1.0, 0.0]),
+                     radius=1.0, material=2, etai_over_etat=1.5)
+    scene.add_sphere(ti.Vector([-4.0, 1., 0.0]), radius=1.0,
+                     material=0, color=ti.Vector([0.4, 0.2, 0.1]))
+    scene.add_sphere(ti.Vector([4., 1., 0.]), 1.0, material=1, color=ti.Vector(
+        [0.7, 0.6, 0.5]), fuzz=0.05)
+
+
+init_scene()
+
+look_from = vec3([16.0, 3.0, 3.0])
+look_at = vec3([0.0, .0, 0.0])
+vup = vec3([0.0, 1.0, 0.0])
+
+aperture = .1
+# dist_to_focus =(look_from - look_at).dot(look_from - look_at)
+dist_to_focus = 10.0
+cam = camera(look_from=look_from, look_at=look_at, vup=vup, vfov=20.0,
+             aspect_ratio=aspect_ratio, focus_dist=dist_to_focus, aperture=aperture)
+
+
+@ti.func
+def ray_color(r) -> ti.Vector:
+    recursive_origin: float_type = r.origin
+    recursive_direction: float_type = r.direction
+    # r = ray(origin=ti.Vector([0.0, 0.0, 0.0]), direction=ti.Vector([1.0, 1.0, 1.0],dt=ti.f32))
+    p_RR = 0.6
+    black = ti.Vector([.0, 0.0, 0.0], dt=float_type)
+    brightness = black
+    if ti.random() < p_RR:
+        brightness = ti.Vector([1.0, 1.0, 1.0], dt=float_type)/p_RR
+        for d in range(depth):
+            # brightness = ti.Vector([0.0, 0.0, 0.0])
+            # break
+            if d == (depth-1):
+                brightness = black
+
+            is_hit, root, position, normal, front_face, color, material, fuzz, etai_over_etat = scene.hit(
+                ray(origin=recursive_origin, direction=recursive_direction), 0.0001, 1000000)
+            # 使用hierarchy 求交
+            t = root
+            if is_hit:
+                if material == 0:
+                    # normal = normal.normalized()
+                    # fac = ti.exp(-root)/4 + 0.75
+                    # brightness *= color * fac + ti.Vector([1.0, 1.0, 1.0], dt=float_type)*(1-fac)
+                    brightness *= color
+                    r_p = random_init_sphere()
+                    recursive_direction = normal.normalized() + r_p
+                    recursive_origin = position
+                elif material == 1:
+                    recursive_direction = reflect(
+                        recursive_direction, normal) + fuzz * random_init_sphere()
+                    recursive_origin = position
+                    brightness *= color
+                elif material == 2:
+                    if d == (depth - 2):
+                        brightness = ti.Vector([1.0, 1.0, 1.0])
+                        break
+                    refraction_ratio = 1.5
+                    if front_face:
+                        refraction_ratio = 0.666
+                    cos_theta = ti.min(-normal.dot(recursive_direction.normalized()), 1.0)
+                    sin_theta = ti.sqrt(1.0 - cos_theta**2)
+                    cannot_refract = refraction_ratio * sin_theta > 1.0
+                    if not cannot_refract or reflectance(cos_theta, refraction_ratio) < ti.random():
+                        recursive_direction = refract(
+                            recursive_direction.normalized(), normal, refraction_ratio)
+                    else:
+                        recursive_direction = reflect(
+                            recursive_direction.normalized(), normal)
+                    recursive_origin = position
+            else:
+                t = 0.5*(r.direction[1] + 1.0)
+                brightness *= (1-t)*ti.Vector([1.0, 1.0, 1.0]
+                                              ) + t*ti.Vector([0.5, 0.7, 1.0])
+                break
+    return brightness
+
+
+@ti.kernel
+def render():
+    print(f'iteration: {iteration[None]}')
+    iteration[None] += 1
+    fa = sampels_per_pixel[None]*iteration[None]
+    for i, j in color_buffer:
+        for s in range(sampels_per_pixel[None]):
+            u = (i+ti.random())/(res[0] - 1)
+            v = (j+ti.random())/(res[1] - 1)
+            color_buffer[i, j] += ray_color(r=cam.get_ray(u, v))
+        frame_buffer[i, j] = color_buffer[i, j] / fa
+        frame_buffer[i, j] = ti.sqrt(frame_buffer[i, j])
+
+
+if output_image:
+    for _ in range(output_image_target_iteration):
+        render()
+    window = ti.GUI(name='weekend', res=res,
+                    background_color=0xAAAAAA, show_gui=False)
+    window.set_image(frame_buffer)
+    window.show(str(int(time.time()))+'.png')
+else:
+    window = ti.GUI(name='weekend', res=res, background_color=0xAAAAAA)
+    while window.running:
+        render()
+        window.set_image(frame_buffer)
+        window.show()
